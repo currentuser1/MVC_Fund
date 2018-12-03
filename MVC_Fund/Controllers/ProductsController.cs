ab2cf72ebb06c0382aed372f509e0a05415c17da
Mikhail <popovms1207@gmail.com>
2018-12-03 14:27:24 +0300

    //

---
+++ MVC_Fund/Controllers/ProductsController.cs
@@ -0,0 +1,55 @@
+﻿using MVC_Fund.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Fund.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public ActionResult List()
+        {
+            return View();
+        }
+        public ActionResult Details()
+        {
+            return View();
+        }
+        public ActionResult Index()
+        {
+            List<Product> products = new List<Product>();
+
+            products.Add(new Product()
+            {
+                Id = 1,
+                Name = "Книга интересная",
+                Description = "Очень интересная кника",
+                Price = 3m
+            });
+
+            products.Add(new Product()
+            {
+                Id = 2,
+                Name = "Бумага A4",
+                Description = "Очень плохая бумага",
+                Price = 15m,
+
+            });
+
+            products.Add(new Product()
+            {
+                Id = 3,
+                Name = "Мобильный телефон",
+                Description = "Раскладушка",
+                Price = 250m,
+            });
+
+            // Возвращаем представление из директории Views/Products/Index.cshtml
+            // Параметр передающийся в метод View() является моделью, которая будет доступна только на чтение в представлении Index
+            return View(products);
+        }
+    }
+}
