efe7078856e12f83bc1400ef0987e025cbe2f768
Mikhail <popovms1207@gmail.com>
2018-12-14 19:28:05 +0300

    //

---
+++ MVC_Fund4_0/Controllers/DataController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Fund4_0.Controllers
+{
+    public class DataController : Controller
+    {
+        // GET: Data
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult RouteInformation()
+        {
+            var z = RouteData.Values["id"];
+            string x;
+            if (z == null) x = "Данные не предоставлены";
+            else x = (string)z;
+            return Content(x, "text/plain", Encoding.UTF8);
+            
+        }
+    }
+}
---
+++ MVC_Fund4_0/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVC_Fund4_0.MvcApplication" Language="C#" %>
