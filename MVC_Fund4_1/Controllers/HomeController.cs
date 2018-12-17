eaf7e9a9c0070306be1461ed314f4f705eaf6b95
Mikhail <popovms1207@gmail.com>
2018-12-17 19:52:50 +0300

    //

---
+++ MVC_Fund4_1/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Fund4_1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Home";
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Title = "About";
+
+            return View();
+        }
+
+        public ActionResult News()
+        {
+            ViewBag.Title = "News";
+
+            return View();
+        }
+    }
+}
