2b65fe85de727a8220314283fe527542373feb19
Mikhail <popovms1207@gmail.com>
2019-01-08 04:02:17 +0300

    //

---
+++ MVC_Fund6_0/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using MVC_Fund6_0.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Fund6_0.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(User incomingData)
+        {
+            Debug.WriteLine("Id = " + incomingData.Id);
+            Debug.WriteLine("Name = " + incomingData.Name);
+            Debug.WriteLine("Surname = " + incomingData.Surname);
+
+            return View();
+        }
+
+    }
+}
---
+++ MVC_Fund6_0/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Fund6_0.Models
+{
+    public class User
+    {
+       
+        
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public string Surname { get; set; }
+        
+    }
+}
