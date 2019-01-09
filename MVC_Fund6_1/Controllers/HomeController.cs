136425e081c05342dfc5541bf1bbde2473251a01
Mikhail <popovms1207@gmail.com>
2019-01-09 03:15:36 +0300

    //

---
+++ MVC_Fund6_1/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using MVC_Fund6_1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Fund6_1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+
+
+
+            User user = new User();
+
+            user.CodeCountry = "+7";
+            user.CodeUser = "123456789";
+
+            user.CodeCity = CityCode.one;
+
+
+
+
+            return View(user);
+
+        }
+
+    }
+}
---
+++ MVC_Fund6_1/Models/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Fund6_1.Models
+{
+    public class User
+    {
+        public string CodeCountry { get; set; }
+
+        public CityCode CodeCity { get; set; }
+        public string CodeUser { get; set; }
+
+        
+    }
+    public enum CityCode
+    {
+        [Display(Name = "495")]
+        one,
+        [Display(Name = "615")]
+        two,
+        [Display(Name = "333")]
+        three
+    }
+}
