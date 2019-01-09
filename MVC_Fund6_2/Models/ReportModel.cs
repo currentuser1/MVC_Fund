82e29ca0e11ea95039320d7030525081f8fa3c7a
Mikhail <popovms1207@gmail.com>
2019-01-10 01:51:34 +0300

    //

---
+++ MVC_Fund6_2/Models/ReportModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Fund6_2.Models
+{
+    public class ReportModel
+    {
+
+        public string StartDate { get; set; }
+        public string EndDate { get; set; }
+
+    }
+}
