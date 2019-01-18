eaa9dba8e9083502afe1f5de51d4fe9c1ca82158
Mikhail <popovms1207@gmail.com>
2019-01-18 17:59:07 +0300

    //

--- MVC_Fund6_2/App_Start/RouteConfig.cs
+++ MVC_Fund6_2/App_Start/RouteConfig.cs
@@ -16,8 +16,21 @@ namespace MVC_Fund6_2
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                namespaces: new[] { "MVC_Fund6_2.Controllers"}
             );
+            routes.MapRoute(
+                name: "PublicRoute",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Orders", action = "Report", y=0 },
+                namespaces: new[] { "MVC_Fund6_2.Controllers" }
+            );
+            routes.MapRoute(
+                 name: "PublicRoute2",
+                 url: "{controller}/{action}/{id}",
+                 defaults: new { controller = "Orders", action = "Report", id = UrlParameter.Optional },
+                 namespaces: new[] { "MVC_Fund6_2.Controllers" }
+);
         }
     }
 }
